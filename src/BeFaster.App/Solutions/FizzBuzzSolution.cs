6a7d3222b487afc9c6cec46d3e320f21541697b2
sbx_user1059 <sbx_user1059@ip-10-11-175-99.eu-west-2.compute.internal>
2018-05-23 16:54:00 +0000

    Wed May 23 16:54:00 UTC 2018

--- src/BeFaster.App/Solutions/FizzBuzzSolution.cs
+++ src/BeFaster.App/Solutions/FizzBuzzSolution.cs
@@ -6,7 +6,10 @@ namespace BeFaster.App.Solutions
     {
         public static string FizzBuzz(int number)
         {
-            throw new SolutionNotImplementedException();
+            if (number % 3 == 0 && number % 5 == 0) return "fizz buzz";
+            if (number % 3 == 0) return "fizz";
+            if (number % 5 == 0) return "buzz";
+            else return "1";
         }
     }
 }
