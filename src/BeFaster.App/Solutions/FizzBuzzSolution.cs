fc11dc2f0e604d3a1e5b6247ff29f66301e8b7e6
sbx_user1059 <sbx_user1059@ip-10-11-175-99.eu-west-2.compute.internal>
2018-05-23 17:24:08 +0000

    Wed May 23 17:24:08 UTC 2018

--- src/BeFaster.App/Solutions/FizzBuzzSolution.cs
+++ src/BeFaster.App/Solutions/FizzBuzzSolution.cs
@@ -6,12 +6,10 @@ namespace BeFaster.App.Solutions
     {
         public static string FizzBuzz(int number)
         {
-            //String containing either the number, "fizz", "buzz" or "fizz buzz"
-            //i think that return any number for example 1 !
+            if ((number % 5 == 0 || number.ToString().Contains("5")) &&
+                (number % 3 == 0 || number.ToString().Contains("3"))) return "fizz buzz";
             if (number % 3 == 0 || number.ToString().Contains("3")) return "fizz";
             if (number % 5 == 0 || number.ToString().Contains("5")) return "buzz";
-            if (number % 5 == 0 || number.ToString().Contains("5") &&
-                number % 3 == 0 || number.ToString().Contains("3")) return "fizz buzz";
             return number.ToString();
         }
     }
