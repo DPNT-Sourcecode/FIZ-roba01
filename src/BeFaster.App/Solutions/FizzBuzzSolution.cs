5a0fa9980810c3b656ee7b3891d2f7ed0ad73d34
sbx_user1059 <sbx_user1059@ip-10-11-175-99.eu-west-2.compute.internal>
2018-05-23 17:16:27 +0000

    Wed May 23 17:16:27 UTC 2018

--- src/BeFaster.App/Solutions/FizzBuzzSolution.cs
+++ src/BeFaster.App/Solutions/FizzBuzzSolution.cs
@@ -6,6 +6,8 @@ namespace BeFaster.App.Solutions
     {
         public static string FizzBuzz(int number)
         {
+            //String containing either the number, "fizz", "buzz" or "fizz buzz"
+            //i think that return any number for example 1 !
             if (number % 3 == 0 && number % 5 == 0) return "fizz buzz";
             if (number % 3 == 0) return "fizz";
             if (number % 5 == 0) return "buzz";
