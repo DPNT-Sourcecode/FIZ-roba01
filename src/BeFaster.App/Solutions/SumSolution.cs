4244cafe4bd347c606c35fed2139d3df8b55f668
sbx_user1059 <sbx_user1059@ip-10-11-175-99.eu-west-2.compute.internal>
2018-05-23 16:32:10 +0000

    Wed May 23 16:32:10 UTC 2018

--- src/BeFaster.App/Solutions/SumSolution.cs
+++ src/BeFaster.App/Solutions/SumSolution.cs
@@ -6,7 +6,9 @@ namespace BeFaster.App.Solutions
     {
         public static int Sum(int x, int y)
         {
-
+            if (x < 0 && x > 100) throw new System.ArgumentException("x should be beetween 0 - 100");
+            if (y < 0 && y > 100) throw new System.ArgumentException("y should be beetween 0 - 100");
+            return x + y;
         }
     }
 }
