5605315b72810bc538267992a2b936db233f401a
sbx_user1059 <sbx_user1059@ip-10-11-175-99.eu-west-2.compute.internal>
2018-05-23 16:56:01 +0000

    Wed May 23 16:56:01 UTC 2018

--- src/BeFaster.App/Solutions/FizzBuzzSolution.cs
+++ src/BeFaster.App/Solutions/FizzBuzzSolution.cs
@@ -9,7 +9,7 @@ namespace BeFaster.App.Solutions
             if (number % 3 == 0 && number % 5 == 0) return "fizz buzz";
             if (number % 3 == 0) return "fizz";
             if (number % 5 == 0) return "buzz";
-            else return "1";
+            return "1";
         }
     }
 }
